845425909a9da11376d41856984ddbb413ab8e0b
tayou89 <118211610+tayou89@users.noreply.github.com>
2024-06-06 01:52:54 +0900

    add nomad-es6

---
+++ Web/javascript/nomad-es6/index.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector(".wrapper");
+
+const addWelcome = () => 
+{
+	const div = `
+		<div class="hello">
+			<h1 class="title">Hello</h1>
+		</div>
+	`;
+	wrapper.innerHTML = div;
+};
+
+setTimeout(addWelcome, 2000);
