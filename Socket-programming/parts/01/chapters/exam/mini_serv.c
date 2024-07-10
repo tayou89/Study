ee40fa2c4c991e0e704b2f1837f9c67e5366f896
tayou89 <118211610+tayou89@users.noreply.github.com>
2024-07-10 12:43:03 +0900

    Add: mini_serv.c

--- Socket-programming/parts/01/chapters/exam/mini_serv.c
+++ Socket-programming/parts/01/chapters/exam/mini_serv.c
@@ -1,112 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
 
-typedef struct s_client
+typedef struct s_socket{
+    int server;
+    int client;
+    int max;
+}   t_socket;
+
+typedef struct s_address {
+    struct sockaddr_in  server;
+    struct sockaddr_in  client;
+    socklen_t           size;
+}   t_address;
+
+typedef struct s_client 
 {
-    int     id;
-    char    msg[100000];
+    int             socket;
+    int             id;
+    struct s_client *next;
 }   t_client;
 
-t_client    clients[1024];
-fd_set      read_set, write_set, current;
-int         maxfd = 0, gid = 0;
-char        send_buffer[120000], recv_buffer[120000];
+typedef struct s_server {
+    t_socket    socket;
+    t_address   address;
+    t_client    *client;
+    int         numberOfClient;
+}   t_server;
 
-void    err(char  *msg)
-{
-    if (msg)
-        write(2, msg, strlen(msg));
-    else
-        write(2, "Fatal error", 11);
-    write(2, "\n", 1);
-    exit(1);
-}
+void    checkArgumentCount(int count);
+void    handleError(char *message);
 
-void    send_to_all(int except)
-{
-    for (int fd = 0; fd <= maxfd; fd++)
-    {
-        if  (FD_ISSET(fd, &write_set) && fd != except)
-            if (send(fd, send_buffer, strlen(send_buffer), 0) == -1)
-                err(NULL);
-    }
-}
+int main(int argc, char *argv[]) {
+    t_server    server;
 
-int     main(int ac, char **av)
-{
-    if (ac != 2)
-        err("Wrong number of arguments");
-
-    struct sockaddr_in  serveraddr;
-    socklen_t           len;
-    int serverfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverfd == -1) err(NULL);
-    maxfd = serverfd;
-
-    FD_ZERO(&current);
-    FD_SET(serverfd, &current);
-    bzero(clients, sizeof(clients));
-    bzero(&serveraddr, sizeof(serveraddr));
-
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serveraddr.sin_port = htons(atoi(av[1]));
-
-    if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(serverfd, 100) == -1)
-        err(NULL);
-
-    while (1)
-    {
-        read_set = write_set = current;
-        if (select(maxfd + 1, &read_set, &write_set, 0, 0) == -1) continue;
-
-        for (int fd = 0; fd <= maxfd; fd++)
-        {
-            if (FD_ISSET(fd, &read_set))
-            {
-                if (fd == serverfd)
-                {
-                    int clientfd = accept(serverfd, (struct sockaddr *)&serveraddr, &len);
-                    if (clientfd == -1) continue;
-                    if (clientfd > maxfd) maxfd = clientfd;
-                    clients[clientfd].id = gid++;
-                    FD_SET(clientfd, &current);
-                    sprintf(send_buffer, "server: client %d just arrived\n", clients[clientfd].id);
-                    send_to_all(clientfd);
-                }
-                else
-                {
-                    int ret = recv(fd, recv_buffer, sizeof(recv_buffer), 0);
-                    if (ret <= 0)
-                    {
-                        sprintf(send_buffer, "server: client %d just left\n", clients[fd].id);
-                        send_to_all(fd);
-                        FD_CLR(fd, &current);
-                        close(fd);
-                    }
-                    else
-                    {
-                        for (int i = 0, j = strlen(clients[fd].msg); i < ret; i++, j++)
-                            {
-                            clients[fd].msg[j] = recv_buffer[i];
-                            if (clients[fd].msg[j] == '\n')
-                            {
-                                clients[fd].msg[j] = '\0';
-                                sprintf(send_buffer, "client %d: %s\n", clients[fd].id, clients[fd].msg);
-                                send_to_all(fd);
-                                bzero(clients[fd].msg, strlen(clients[fd].msg));
-                                j = -1;
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-        }
-    }
+    checkArgumentCount(argc);
+    initializeServer(&server);
     return (0);
+}
+
+void    checkArgumentCount(int count) {
+    if (count < 2)
+        handleError("Wrong number of arguments");
+}
+
+void    initializeServer(t_server *server) {
+    server->socket.server = socket(PF_INET, SOCK_STREAM, 0);
+    if (server->socket.server == -1)
+        handleError("Fatal error");
+    server->socket.client = -1;
+    server->socket.max = server->socket.server;
+    memset(&server->address.server, 0, sizeof(server->address.server));
+    memset(&server->address.client, 0, sizeof(server->address.client));
+    server->address.size = sizeof(server->address.client);
+}
+
+void    handleError(char *message) {
+    write(STDERR_FILENO, message, strlen(message));
+    write(STDERR_FILENO, '\n', 1);
+    exit(1);
 }
