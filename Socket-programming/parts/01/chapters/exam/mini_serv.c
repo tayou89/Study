ac3cb2dd59a6fe7b800cd104763b683fdddc3141
tayou89 <118211610+tayou89@users.noreply.github.com>
2024-07-10 14:34:54 +0900

    add initializing functions

--- Socket-programming/parts/01/chapters/exam/mini_serv.c
+++ Socket-programming/parts/01/chapters/exam/mini_serv.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
+#include <sys/select.h>
 
 typedef struct s_socket{
     int server;
@@ -26,17 +28,25 @@ typedef struct s_server {
     t_socket    socket;
     t_address   address;
     t_client    *client;
+    fd_set      fdSet;
     int         numberOfClient;
 }   t_server;
 
 void    checkArgumentCount(int count);
 void    handleError(char *message);
+void    initializeServer(t_server *server, int port);
+void    initializeSocket(t_socket *sockets);
+void    initializeAddress(t_address *address, int port);
+void    initializeFdSet(fd_set *fdSet, int serverSocket);
+void    makeServerSocketListen(int socket, struct sockaddr_in address);
 
 int main(int argc, char *argv[]) {
     t_server    server;
 
     checkArgumentCount(argc);
-    initializeServer(&server);
+    initializeServer(&server, atoi(argv[1]));
+    makeServerSocketListen(server.socket.server, server.address.server);
+    makeChattingLoop(&server);
     return (0);
 }
 
@@ -45,16 +55,54 @@ void    checkArgumentCount(int count) {
         handleError("Wrong number of arguments");
 }
 
-void    initializeServer(t_server *server) {
-    server->socket.server = socket(PF_INET, SOCK_STREAM, 0);
-    if (server->socket.server == -1)
-        handleError("Fatal error");
-    server->socket.client = -1;
-    server->socket.max = server->socket.server;
-    memset(&server->address.server, 0, sizeof(server->address.server));
-    memset(&server->address.client, 0, sizeof(server->address.client));
-    server->address.size = sizeof(server->address.client);
+void    initializeServer(t_server *server, int port) {
+    initializeSocket(&server->socket);
+    initializeAddress(&server->address, port);
+    initializeFdSet(&server->fdSet, server->socket.server);
 }
+
+void    initializeSocket(t_socket *sockets) {
+    sockets->server = socket(PF_INET, SOCK_STREAM, 0);
+    if(sockets->server == -1)
+        handleError("Fatal error");
+    sockets->client = -1;
+    sockets->max = sockets->server;
+}
+
+void    initializeAddress(t_address *address, int port) {
+    memset(&address->server, 0, sizeof(address->server));
+    address->server.sin_family = PF_INET;
+    address->server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    address->server.sin_port = htons(port);
+    memset(&address->client, 0, sizeof(address->client));
+    address->size = sizeof(address->client);
+}
+
+void    initializeFdSet(fd_set *fdSet, int serverSocket) {
+    FD_ZERO(&fdSet);
+    FD_SET(serverSocket, &fdSet);
+}
+
+void    makeServerSocketListen(int socket, struct sockaddr_in address) {
+    if (bind(socket, &address, sizeof(address)) == -1)
+        handleError("Fatal error");
+    if (listen(socket, 10) == -1)
+        handleError("Fatal error");
+}
+
+void    makeChattingLoop(t_server *server) {
+    int     numberOfSelectedFds;
+    fd_set  readSet;
+    fd_set  writeSet;
+
+    while (1) {
+        readSet = server->fdSet;
+        writeSet = server->fdSet;
+        numberOfSelectedFds = select(server->socket.max + 1, &readSet, &writeSet, 0, 0);
+        if (numberOfSelectedFds <= 0) 
+            continue ;
+    }
+} 
 
 void    handleError(char *message) {
     write(STDERR_FILENO, message, strlen(message));
