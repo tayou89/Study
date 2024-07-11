f61fdcbb03b87d938cbe7967a33055ea6d078ead
tayou89 <118211610+tayou89@users.noreply.github.com>
2024-07-11 20:18:53 +0900

    Add: receiving message function

--- Socket-programming/parts/01/chapters/exam/main.c
+++ Socket-programming/parts/01/chapters/exam/main.c
@@ -3,11 +3,13 @@
 
 int	main(void) {
 	char	string[10];
-	char	*example1 = "abcd";
-	char	*example2 = "ggggg";
+	char	*example1 = "abcdefghijk";
+	// char	*example2 = "ggggg";
 
 	strcpy(string, example1);
+	string[sizeof(string) - 1] = '\0';
 	printf("string: %s\n", string);
-	strcat(string, example2);
+	// strcat(string, example2);
+	// printf("string: %s\n", string);
 	return (0);
 }
--- Socket-programming/parts/01/chapters/exam/mini_serv.c
+++ Socket-programming/parts/01/chapters/exam/mini_serv.c
@@ -6,62 +6,48 @@
 #include <sys/select.h>
 
 #define BUFFER_SIZE	1024
+#define CLIENTS_MAX  1024
 
-typedef struct s_socket{
-    int server;
-    int client;
-    int max;
-}   t_socket;
-
-typedef struct s_address {
-    struct sockaddr_in  server;
-    struct sockaddr_in  client;
-    socklen_t           size;
-}   t_address;
-
-typedef struct s_client {
-    int             socket;
-    int             id;
-    struct s_client *next;
-    struct s_client *prev;
-}   t_client;
-
-typedef struct s_fdSet {
-    fd_set  origin;
-    fd_set  read;
-    fd_set  write;
-}   t_fdSet;
+typedef struct s_request {
+	int		socket;
+	int		id;
+	char	*message;
+}	t_request;
 
 typedef struct s_server {
-    t_socket    socket;
-    t_address   address;
-    t_fdSet     fdSet;
-    t_client    *clientList;
-    int         lastClientId;
+    int 				socket;
+    int					fdMax;
+    int					clientIds[CLIENTS_MAX];
+    int					lastClientId;
+	fd_set				fdSet;
+	struct sockaddr_in	address;
+	t_request			request;
 }   t_server;
 
-void    checkArgumentCount(int count);
-void    handleError(char *message, t_server *server);
-void    initializeServer(t_server *server, int port);
-void    initializeSocket(t_socket *sockets);
-void    initializeAddress(t_address *address, int port);
-void    initializeFdSet(t_fdSet *fdSet, int serverSocket);
-void    makeServerSocketListen(int socket, struct sockaddr_in address);
-void    makeChattingLoop(t_server *server);
-void    acceptClient(t_server *server);
-t_client	*makeClient(t_server *server);
-void	addClient(t_client **clientList, t_client *newClient);
-void	receiveClientMessage(t_server *server);
-void	removeClient(t_server *server);
-void	sendMessage(char *message, t_server *server);
+void    			checkArgumentCount(int count);
+void				initializeServer(t_server *server, int port);
+int					kgetServerSocket(void);
+fd_set				getServerFdSet(int socket);
+struct sockaddr_in	getServerAddress(int port);
+void    			makeServerSocketListen(int socket, struct sockaddr_in address);
+void    			makeChattingLoop(t_server *server);
+void    			acceptClient(t_server *server);
+void				receiveRequestMessage(t_server *server);
+void				removeClient(int clientSocket, t_server *server);
+int					getSocketMax(int prevMax, fd_set fdSet);
+void				getRequestMessage(char *prevMessage, t_server *server);
+void				sendClientMessage(t_server *server);
+void				sendMessage(char *message, int ignoreFd, t_server server); 
+void				handleError(char *message, t_server *server);
+void				freeRequestMessage(t_server *server);
 
 int main(int argc, char *argv[]) {
     t_server    server;
 
     checkArgumentCount(argc);
-    initializeServer(&server, atoi(argv[1]));
-    makeServerSocketListen(server.socket.server, server.address.server);
+    setServer(&server, atoi(argv[1]));
     makeChattingLoop(&server);
+	close(server.socket);
     return (0);
 }
 
@@ -70,176 +56,148 @@ void    checkArgumentCount(int count) {
         handleError("Wrong number of arguments", NULL);
 }
 
-void    initializeServer(t_server *server, int port) {
-    initializeSocket(&server->socket);
-    initializeAddress(&server->address, port);
-    initializeFdSet(&server->fdSet, server->socket.server);
-	server->clientList = NULL;
+void    setServer(t_server *server, int port) {
+	server->socket = getServerSocket();
+	server->fdSet = getServerFdSet(server->socket);
+	server->fdMax = server->socket;
+	server->address = getServerAddress(port);
+	makeServerSocketListen(server->socket, server->address);
 	server->lastClientId = -1;
+	server->request.socket = -1;
+	server->request.id = -1;
+	server->request.message = NULL;
 }
 
-void    initializeSocket(t_socket *sockets) {
-    sockets->server = socket(PF_INET, SOCK_STREAM, 0);
-    if(sockets->server == -1)
-        handleError("Fatal error", NULL);
-    sockets->client = -1;
-    sockets->max = sockets->server;
+int	getServerSocket(void) {
+	int serverSocket;
+
+	serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (serverSocket == -1)
+		handleError("Fatal error", NULL);
+	return (serverSocket);
 }
 
-void    initializeAddress(t_address *address, int port) {
-    memset(&address->server, 0, sizeof(address->server));
-    address->server.sin_family = PF_INET;
-    address->server.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    address->server.sin_port = htons(port);
-    memset(&address->client, 0, sizeof(address->client));
-    address->size = sizeof(address->client);
+fd_set	getServerFdSet(int socket) {
+	fd_set	fdSet;
+
+	FD_ZERO(&fdSet);
+	FD_SET(socket, &fdSet);
+	return (fdSet);
 }
 
-void    initializeFdSet(t_fdSet *fdSet, int serverSocket) {
-    FD_ZERO(&fdSet->origin);
-    FD_SET(serverSocket, &fdSet->origin);
+struct sockaddr_in	getServerAddress(int port) {
+	struct sockaddr_in	address;
+
+	memset(&address, 0, sizeof(address));
+	address.sin_family = PF_INET;
+	address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	address.sin_port = htons(port);
+	return (address);
 }
 
 void    makeServerSocketListen(int socket, struct sockaddr_in address) {
-    if (bind(socket, &address, sizeof(address)) == -1)
+    if (bind(socket, (struct sockaddr *) &address, sizeof(address)) == -1)
         handleError("Fatal error", NULL);
-    if (listen(socket, 10) == -1)
+    if (listen(socket, 100) == -1)
         handleError("Fatal error", NULL);
 }
 
 void    makeChattingLoop(t_server *server) {
-    int     numberOfSelectedFds;
+    int		fdCount = 0;
+	fd_set	readSet = server->fdSet;
 
     while (1) {
-        numberOfSelectedFds = 
-            select(server->socket.max + 1, &server->fdSet.read, &server->fdSet.write, 0, 0);
-        if (numberOfSelectedFds)
-            responseFromSelectedFds(server);
+        fdCount = select(server->fdMax, &readSet, 0, 0, 0);
+        if (fdCount)
+            response(server, readSet);
+		readSet = server->fdSet;
     }
 } 
 
-void    responseFromSelectedFds(t_server *server) {
-    int i;
+void    response(t_server *server, fd_set readSet) {
+    int 	i;
 
-    for (i = 0; i < server->socket.max; i++) {
-        if (FD_ISSET(i, &server->fdSet.read)) {
-            if (i == server->socket.server)
+    for (i = 0; i < server->fdMax + 1; i++) {
+        if (FD_ISSET(i, &readSet)) {
+            if (i == server->socket)
                 acceptClient(server);
-			else {
-				server->socket.client = i;
-				receiveClientMessage(server);
+			else  {
+				server->request.socket = i;
+				server->request.id = server->clientIds[i];
+				receiveRequestMessage(server);
+				sendClientMessage(server);
 			}
         }
     }
 }
 
 void    acceptClient(t_server *server) {
-	int			stringSize;
-    t_client    *newClient;
-	char		buffer[1000];
+	char	buffer[BUFFER_SIZE];
+    int     clientSocket;
 
-    server->socket.client = 
-        accept(server->socket.server, (struct sockaddr *) &server->address.client, &server->address.size);
-    FD_SET(server->socket.client, &server->fdSet.origin);
-    if (server->socket.client > server->socket.max)
-        server->socket.max = server->socket.client;
-	newClient = makeClient(server);
-	addClient(&server->clientList, newClient);
-	sprintf(buffer, "server: client %d just arrived\n", newClient->id);
-	sendMessage(server, newClient->id);
+    clientSocket = accept(server->socket, NULL, NULL);
+    FD_SET(clientSocket, &server->fdSet);
+    if (clientSocket > server->fdMax)
+        server->fdMax = clientSocket;
+	server->clientIds[clientSocket] = server->lastClientId + 1;
+	server->lastClientId = server->clientIds;
+	sprintf(buffer, "server: client %d just arrived\n", server->clientIds[clientSocket]);
+	sendMessage(buffer, clientSocket, *server);
 }
 
-t_client	*makeClient(t_server *server) {
-	t_client	*newClient;
-
-	newClient = (t_client *) malloc(sizeof(t_client));
-	newClient->socket = server->socket.client;
-	newClient->id = ++(server->lastClientId);
-	newClient->next = NULL;
-	newClient->prev = NULL;
-	if (!newClient)
-		handleError("Fatal error", NULL);
-	return (newClient);
-}
-
-void	addClient(t_client **clientList, t_client *newClient) {
-	t_client	*copyList;
-
-	if (*clientList == NULL)
-		*clientList = newClient;
-	else {
-		copyList = *clientList;
-		while (copyList->next)
-			copyList = copyList->next;
-		copyList->next = newClient;
-		newClient->prev = copyList;
-	}
-}
-
-void	receiveClientMessage(t_server *server) {
-	char	*clientMessage;
+void	receiveRequestMessage(t_server *server) {
 	char	buffer[BUFFER_SIZE];
 	int		readSize;
 
-	readSize = recv(server->socket.client, buffer, BUFFER_SIZE - 1, 0);
+	readSize = recv(server->request.socket, buffer, BUFFER_SIZE - 1, 0);
 	buffer[readSize] = '\0';
 	if (readSize == 0) 
-		removeClient(server);
-	else  {
-		clientMessage = getClientMessage(server);
-	}
+		removeClient(server->request.socket, server);
+	else 
+		getRequestMessage(buffer, server);
 }
 
-void	removeClient(t_server *server) {
-	t_client	*client;
+void	removeClient(int clientSocket, t_server *server) {
 	char		buffer[BUFFER_SIZE];
-	int			clientSocket = server->socket.client;
 
-	client = server->clientList;
-	while (client && client->socket != clientSocket)
-		client = client->next;
-	if (client->prev)
-		client->prev->next = client->next;
-	if (client->next)
-		client->next->prev = client->prev;
-	if (client == server->clientList)
-		server->clientList = client->next;
-	if (clientSocket == server->socket.max)
-		server->socket.max = getSocketMax(server);
-	sprintf(buffer, "server: client %d just left\n", client->id);
-	FD_CLR(clientSocket, &server->fdSet.origin);
+	FD_CLR(clientSocket, &server->fdSet);
 	close(clientSocket);
-	free(client);
-	client = NULL;
+	if (clientSocket == server->fdMax)
+		server->fdMax = getNewSocketMax(clientSocket, server->fdSet);
+	sprintf(buffer, "server: client %d just left\n", server->clientIds[clientSocket]);
+	sendMessage(buffer, clientSocket, *server);
 }
 
-int	getSocketMax(t_server *server) {
-	int			max = server->socket.server;
-	t_client	*client;
+int	getSocketMax(int prevMax, fd_set fdSet) {
+	int		max = 0;
+	int		i;
 
-	client = server->clientList;	
-	while (client) {
-		if (client->socket > max)
-			max = client->socket;
-		client = client->next;
+	for (i = 0; i < prevMax; i++) {
+		if (FD_ISSET(i, &fdSet) && i > max)
+			max = i;
 	}
 	return (max);
 }
 
-void	getClientMessage(t_server *server, char *buffer) {
-	char	*clientMessage;
-	char	*temp;
+void	getRequestMessage(char *prevMessage, t_server *server) {
+	char	buffer[BUFFER_SIZE];
+	char	*joinedMessage;
 	int		recvSize;
 
-	clientMessage = (char *) malloc(sizeof(char) * strlen(buffer) + 1);
-	if (!clientMessage)
-		handleError("Fatal error", server);
+	server->request.message = (char *) malloc(sizeof(char) * strlen(prevMessage) + 1);
+	if (!server->request.message)
+		handleError("Fatal error", NULL);
 	bzero(buffer, BUFFER_SIZE);
-	recvSize = recv(server->socket.client, buffer, BUFFER_SIZE - 1, 0);
-	buffer[recvSize] = '\0';
+	recvSize = recv(server->request.socket, buffer, BUFFER_SIZE - 1, 0);
 	while (recvSize) {
-		temp = clientMessage;
-		clientMessage = ft_strjoin(clientMessage, buffer);
+		joinedMessage = ft_strjoin(server->request.message, buffer);
+		if (!joinedMessage)
+			handleError("Fatal error", server);
+		free(server->request.message);
+		server->request.message = NULL;
+		server->request.message = joinedMessage;
+		bzero(buffer, BUFFER_SIZE);
+		recvSize = recv(server->request.socket, buffer, BUFFER_SIZE - 1, 0);
 	}
 }
 
@@ -250,36 +208,64 @@ char	*ft_strjoin(char *string1, char *string2) {
 	joinedString = (char *) malloc(sizeof(char) * size + 1);
 	if (!joinedString)	
 		return (NULL);
+	strcpy(joinedString, string1);
+	strcat(joinedString, string2);
+	joinedString[size] = '\0';
+	return (joinedString);
 }
 
-void	sendMessage(char *message, t_server *server) {
+void	sendClientMessage(t_server *server) {
+	char	messageLine[strlen(server->request.message)];
+	char	*buffer;
+	char	*start = server->request.message;
+	char	*end = start + strlen(start);
+	char	*newLine = NULL;
+
+	while (*start != '\0') {
+		newLine = strstr(start, "\n");
+		if (newLine)
+			end = newLine + 1;
+		else
+			end = start + strlen(start);
+		strcpy(messageLine, start);
+		messageLine[end - start] = '\0';
+		buffer = (char *) malloc(strlen(messageLine) + 100);
+		if (!buffer) 
+			handleError("Fatal error", server);
+		sprintf(buffer, "client %d: %s", server->request.id, server->request.message);
+		sendMessage(buffer, server->request.socket, *server);
+		free(buffer);
+		buffer = NULL;
+		start = end + 1;
+	}
+	freeRequestMessage(server);
+}
+
+void	sendMessage(char *message, int ignoreFd, t_server server) {
+	fd_set			writeSet = server.fdSet;
+	struct timeval	timeout;
 	int	i;
 
-	for (i = 0; i < server->socket.max + 1; i++) {
-		if (FD_ISSET(i, &server->fdSet.write)) {
-			if (i != server->socket.server && i != server->socket.client)
-				send(i, server->buffer, strlen(server->buffer), 0);
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 0;
+	select(server.fdMax + 1, 0, &writeSet, 0, &timeout);
+	for (i = 0; i < server.fdMax + 1; i++) {
+		if (FD_ISSET(i, &writeSet)) {
+			if (i != server.socket && i != ignoreFd)
+				send(i, message, strlen(message), 0);
 		}
 	}
 }
 
 void    handleError(char *message, t_server *server) {
-	if (server)
-		freeAllocatedData(server);
+	if (server && server->request.message) 
+		freeRequestMessage(server);
     write(STDERR_FILENO, message, strlen(message));
     write(STDERR_FILENO, '\n', 1);
     exit(1);
 }
 
-void	freeAllocatedData(t_server *server) {
-	t_client	*client = server->clientList;
-	t_client	*temp;
-
-	while (client) {
-		temp = client;
-		client = client->next;
-		free(temp);
-		temp = NULL;
-	}
-	server->clientList = NULL;
+void	freeRequestMessage(t_server *server) {
+	free(server->request.message);
+	server->request.message = NULL;
 }
